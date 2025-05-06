427750fccb4024ec8378d814077b610b5cb69580
Jaraí Santos <jaraisantos88@gmail.com>
2025-05-06 20:21:47 -0300

    Add files via upload

---
+++ ex 2.2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int main(){
+	int idade = 0;
+	int ano_de_nascimento = 2006;
+	
+	printf ("Digite sua idade: \n");
+	scanf ("%d", &idade);
+	printf ("Idade informada: %d.\n", idade);
+	
+	printf ("Digite seu ano de nascimento: \n");
+	scanf ("%d", &ano_de_nascimento);
+	printf ("ano_de_nascimento: %d.\n", ano_de_nascimento);
+	
+	
+	
+
+}
